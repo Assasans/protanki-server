8c3593c0d60e360532673d6312006e7497691bbc
Daniil Pryima <swimmin2@gmail.com>
2022-03-15 09:11:25 +0200

    style: Remove redundant empty lines

--- src/main/kotlin/jp/assasans/protanki/server/client/UserSocket.kt
+++ src/main/kotlin/jp/assasans/protanki/server/client/UserSocket.kt
@@ -501,17 +501,11 @@ class UserSocket(
       val userItem = user.items.singleOrNull { it.marketItem == marketItem }
       val clientMarketItems = when(marketItem) {
         is ServerGarageItemWeapon       -> garageItemConverter.toClientWeapon(marketItem)
-
         is ServerGarageItemHull         -> garageItemConverter.toClientHull(marketItem)
-
         is ServerGarageItemPaint        -> listOf(garageItemConverter.toClientPaint(marketItem))
-
         is ServerGarageItemSupply       -> listOf(garageItemConverter.toClientSupply(marketItem))
-
         is ServerGarageItemSubscription -> listOf(garageItemConverter.toClientSubscription(marketItem))
-
         is ServerGarageItemKit          -> listOf(garageItemConverter.toClientKit(marketItem))
-
         is ServerGarageItemPresent      -> listOf(garageItemConverter.toClientPresent(marketItem))
 
         else                            -> throw NotImplementedError("Not implemented: ${marketItem::class.simpleName}")
