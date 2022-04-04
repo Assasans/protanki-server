ba1cb02558cc914fc15b577ad2cbabe6f831a809
Daniil Pryima <swimmin2@gmail.com>
2022-04-04 09:29:05 +0300

    feat(resources): Autodetect resources directory

--- src/main/kotlin/jp/assasans/protanki/server/ResourceManager.kt
+++ src/main/kotlin/jp/assasans/protanki/server/ResourceManager.kt
@@ -2,13 +2,22 @@ package jp.assasans.protanki.server
 
 import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.io.path.exists
 
 interface IResourceManager {
   fun get(path: String): Path
 }
 
 class ResourceManager : IResourceManager {
-  private val resourceDirectory: Path = Paths.get("data")
+  private val resourceDirectory: Path
+
+  init {
+    var directory = Paths.get("data")
+    if(!directory.exists()) directory = Paths.get("src/main/resources/data") // Started from IntelliJ IDEA, default working directory
+    if(!directory.exists()) throw Exception("Cannot find runtime resources directory")
+
+    resourceDirectory = directory
+  }
 
   override fun get(path: String): Path {
     return resourceDirectory.resolve(path)
