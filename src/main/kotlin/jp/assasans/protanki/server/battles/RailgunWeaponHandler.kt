f41fbc3ce1123f5b3bfd5aa65afb43d3862f46a9
Daniil Pryima <swimmin2@gmail.com>
2022-06-01 10:54:31 +0300

    feat(battle/weapon): Add Railgun damage handler
    This is a bare handler that has fixed damage.

--- src/main/kotlin/jp/assasans/protanki/server/battles/RailgunWeaponHandler.kt
+++ src/main/kotlin/jp/assasans/protanki/server/battles/RailgunWeaponHandler.kt
@@ -1,5 +1,6 @@
 package jp.assasans.protanki.server.battles
 
+import kotlin.math.ceil
 import jp.assasans.protanki.server.client.railgun.FireTarget
 import jp.assasans.protanki.server.client.toJson
 import jp.assasans.protanki.server.commands.Command
@@ -17,8 +18,23 @@ class RailgunWeaponHandler(
   }
 
   suspend fun fireTarget(target: FireTarget) {
-    val tank = player.tank ?: throw Exception("No Tank")
+    val sourceTank = player.tank ?: throw Exception("No Tank")
+    val battle = player.battle
 
-    Command(CommandName.ShotTarget, listOf(tank.id, target.toJson())).sendTo(tank.player.battle)
+    // Preserve order of targets
+    // TODO(Assasans): Replace with a more efficient algorithm
+    val targetTanks = target.targets
+      .mapNotNull { username -> battle.players.singleOrNull { player -> player.user.username == username } }
+      .mapNotNull { player -> player.tank }
+      .filter { tank -> target.targets.contains(tank.id) }
+      .filter { tank -> tank.state == TankState.Active }
+
+    var damage = 70.0
+    targetTanks.forEach { targetTank ->
+      battle.damageProcessor.dealDamage(sourceTank, targetTank, damage, isCritical = false)
+      damage = ceil(damage * 0.5)
+    }
+
+    Command(CommandName.ShotTarget, listOf(sourceTank.id, target.toJson())).sendTo(player.battle)
   }
 }
