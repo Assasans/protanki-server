1757b8089eb75fff9dad97b4b09c267b1276682e
Daniil Pryima <swimmin2@gmail.com>
2022-08-01 13:19:38 +0000

    feat(battle/bonus): Repair kit incremental heal
    Repair kit heals 1500 HP immediately,
    and 300 HP every 500 ms for 3 seconds

--- src/main/kotlin/jp/assasans/protanki/server/battles/effect/RepairKitEffect.kt
+++ src/main/kotlin/jp/assasans/protanki/server/battles/effect/RepairKitEffect.kt
@@ -1,13 +1,16 @@
 package jp.assasans.protanki.server.battles.effect
 
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
 import jp.assasans.protanki.server.battles.BattleTank
 
 class RepairKitEffect(
   tank: BattleTank
 ) : TankEffect(
   tank,
-  duration = 2.seconds,
+  duration = 3.seconds,
   cooldown = 20.seconds
 ) {
   override val info: EffectInfo
@@ -20,6 +23,19 @@ class RepairKitEffect(
     val battle = tank.battle
     val damageProcessor = battle.damageProcessor
 
-    damageProcessor.heal(tank, 150.0) // TODO(Assasans)
+    // TODO(Assasans): More complicated logic
+    damageProcessor.heal(tank, 1500.0)
+
+    if(duration == null) return
+    tank.coroutineScope.launch {
+      val startTime = Clock.System.now()
+      val endTime = startTime + duration
+      while(Clock.System.now() < endTime) {
+        delay(500)
+        if(tank.health < tank.maxHealth) {
+          damageProcessor.heal(tank, 300.0)
+        }
+      }
+    }
   }
 }
