65fd83f783b5575c9faba67576091079b4ef604d
Daniil Pryima <swimmin2@gmail.com>
2022-08-29 15:48:53 +0200

    fix(utils): Fix ByteArray.indexOfSequence()

--- src/main/kotlin/jp/assasans/protanki/server/extensions/ByteArray.kt
+++ src/main/kotlin/jp/assasans/protanki/server/extensions/ByteArray.kt
@@ -1,22 +1,20 @@
 package jp.assasans.protanki.server.extensions
 
 fun ByteArray.indexOfSequence(sequence: ByteArray, startFrom: Int = 0): Int {
+  if(startFrom < 0) throw IllegalArgumentException("startFrom must be >= 0")
   if(this.isEmpty()) return -1
   if(sequence.isEmpty()) return 0
-  if(startFrom < 0) throw IllegalArgumentException("startFrom must be >= 0")
-  if(startFrom >= this.size) return -1
+  if(startFrom + sequence.size >= this.size) return -1
 
-  var matchOffset = 0
-  var start = startFrom
-  var offset = start
-  while(offset < size) {
-    if(this[offset] == sequence[matchOffset]) {
-      if(matchOffset++ == 0) start = offset
-      if(matchOffset == sequence.size) return start
-    } else {
-      matchOffset = 0
-    }
-    offset++
+  var index = startFrom
+  var matchIndex = 0
+  while(index < size) {
+    val value = this[index++]
+
+    if(value != sequence[matchIndex]) matchIndex = 0
+    if(value == sequence[matchIndex]) matchIndex++
+
+    if(matchIndex == sequence.size) return index - matchIndex
   }
 
   return -1
