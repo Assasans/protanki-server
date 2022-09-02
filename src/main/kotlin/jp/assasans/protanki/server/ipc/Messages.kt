f8974f626593c692e464cd969503c560ef7242d0
Daniil Pryima <swimmin2@gmail.com>
2022-09-02 14:59:04 +0200

    feat(ipc): Remove IPC invite management
    REST API server should be used to manage invites.

--- src/main/kotlin/jp/assasans/protanki/server/ipc/Messages.kt
+++ src/main/kotlin/jp/assasans/protanki/server/ipc/Messages.kt
@@ -13,20 +13,3 @@ class ServerStartedMessage : ProcessMessage()
 
 class ServerStopRequest : ProcessMessage()
 class ServerStopResponse : ProcessMessage()
-
-class InviteToggleRequest(@Json val enabled: Boolean) : ProcessMessage()
-class InviteToggleResponse : ProcessMessage()
-
-class InviteAddRequest(@Json val code: String) : ProcessMessage()
-class InviteAddResponse(@Json val success: Boolean) : ProcessMessage()
-
-class InviteDeleteRequest(@Json val code: String) : ProcessMessage()
-class InviteDeleteResponse(@Json val success: Boolean) : ProcessMessage()
-
-class InviteListRequest : ProcessMessage()
-class InviteListResponse(@Json val invites: List<Invite>) : ProcessMessage() {
-  data class Invite(
-    @Json val id: Int,
-    @Json val code: String
-  )
-}
