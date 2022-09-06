a40f70c90045d72804114318ee9b94d447c681c1
Daniil Pryima <swimmin2@gmail.com>
2022-09-06 15:21:27 +0200

    feat(utils): Predicates for Iterable extensions

--- src/main/kotlin/jp/assasans/protanki/server/extensions/Iterable.kt
+++ src/main/kotlin/jp/assasans/protanki/server/extensions/Iterable.kt
@@ -1,8 +1,11 @@
 package jp.assasans.protanki.server.extensions
 
 // TODO(Assasans): Find a way to not specify the type of the Iterable<T> in calls
-inline fun <T, reified I : T> Iterable<T>.singleOf(): I? = this.single { it is I }?.cast()
-inline fun <T, reified I : T> Iterable<T>.singleOrNullOf(): I? = this.singleOrNull { it is I }?.cast()
+inline fun <T : Any, reified I : T> Iterable<T>.singleOf(predicate: (T) -> Boolean = { true }): I =
+  single { it is I && predicate(it) }.cast()
+
+inline fun <T : Any, reified I : T> Iterable<T>.singleOrNullOf(predicate: (T) -> Boolean = { true }): I? =
+  singleOrNull { it is I && predicate(it) }?.cast()
 
 /**
  * Returns the single element matching the given [predicate],
@@ -10,7 +13,7 @@ inline fun <T, reified I : T> Iterable<T>.singleOrNullOf(): I? = this.singleOrNu
  * or throws exception if there is more than one matching element.
  */
 inline fun <T> Iterable<T>.singleOrNullOrThrow(predicate: (T) -> Boolean = { true }): T? {
-  val items = this.filter(predicate)
+  val items = filter(predicate)
   if(items.isEmpty()) return null
   return items.single()
 }
